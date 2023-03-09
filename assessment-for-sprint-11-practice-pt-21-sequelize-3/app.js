490bc9cc1f64d5eb075f5fe6dbee4454ad230a3e
Emir <emirusubaliev@Emirs-MacBook-Pro.local>
2023-03-09 17:17:38 -0600

    phase3 done

--- assessment-for-sprint-11-practice-pt-21-sequelize-3/app.js
+++ assessment-for-sprint-11-practice-pt-21-sequelize-3/app.js
@@ -2,11 +2,41 @@ require('express-async-errors');
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const { WarehouseItem } = require('./db/models')
 
 app.use(express.json());
 
+
+app.get('/items', async (req, res, next) => {
+  const newItem = await WarehouseItem.findAll({where:{isUsed:false}})
+  res.json(newItem)
+})
+
+app.put('/items/:itemId', async (req, res, next) => {
+  try {
+    const newItem = await WarehouseItem.findByPk(req.params.itemId)
+    const {name, price, quantity, isUsed} = req.body
+    newItem.name = name
+    newItem.price = price
+    newItem.quantity = quantity
+    newItem.isUsed = isUsed
+
+    newItem.save()
+    res.json(newItem)
+  } catch (error) {
+    res.statusCode = 404
+    res.json({
+      message: "Warehouse Item not found"
+    })
+  }
+})
+
+app.get('items/:name', async (req, res, next) => {
+  
+})
+
 if (require.main === module) {
-  const port = 8003;
+  const port = 3000;
   app.listen(port, () => console.log('Server-3 is listening on port', port));
 } else {
   module.exports = app;
